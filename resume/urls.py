2cc2b681ec8c9955fe333e10ed18b41f3c0b5d7c
dedeansr <dedeanshori.id@gmail.com>
2025-03-15 15:08:36 +0800

    added models & apps pengguna and berita

--- resume/urls.py
+++ resume/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from resume.views import home, about
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
     path('about/', about, name="about"),
 ]
+
+if settings.DEBUG: 
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
